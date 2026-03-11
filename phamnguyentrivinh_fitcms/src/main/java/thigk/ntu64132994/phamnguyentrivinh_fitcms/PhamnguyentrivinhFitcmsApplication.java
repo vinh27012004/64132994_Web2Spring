57f6cfe7029ad5f241e231c192d10f62d7ffe102
Phạm Nguyễn Trí Vinh <115800576+vinh27012004@users.noreply.github.com>
2026-03-11 14:43:13 +0700

    ntuCMS

---
+++ phamnguyentrivinh_fitcms/src/main/java/thigk/ntu64132994/phamnguyentrivinh_fitcms/PhamnguyentrivinhFitcmsApplication.java
@@ -0,0 +1,13 @@
+package thigk.ntu64132994.phamnguyentrivinh_fitcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamnguyentrivinhFitcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamnguyentrivinhFitcmsApplication.class, args);
+	}
+
+}
---
+++ phamnguyentrivinh_fitcms/src/test/java/thigk/ntu64132994/phamnguyentrivinh_fitcms/PhamnguyentrivinhFitcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu64132994.phamnguyentrivinh_fitcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamnguyentrivinhFitcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
