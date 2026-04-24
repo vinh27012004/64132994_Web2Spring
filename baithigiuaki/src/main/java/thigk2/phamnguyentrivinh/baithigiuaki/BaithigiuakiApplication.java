0296af3068c9cc233a5b44c4ba340f36e8313e08
Phạm Nguyễn Trí Vinh <115800576+vinh27012004@users.noreply.github.com>
2026-04-24 12:20:09 +0700

    PUSH LAN 1

---
+++ baithigiuaki/src/main/java/thigk2/phamnguyentrivinh/baithigiuaki/BaithigiuakiApplication.java
@@ -0,0 +1,13 @@
+package thigk2.phamnguyentrivinh.baithigiuaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaithigiuakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaithigiuakiApplication.class, args);
+	}
+
+}
---
+++ baithigiuaki/src/test/java/thigk2/phamnguyentrivinh/baithigiuaki/BaithigiuakiApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.phamnguyentrivinh.baithigiuaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaithigiuakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
