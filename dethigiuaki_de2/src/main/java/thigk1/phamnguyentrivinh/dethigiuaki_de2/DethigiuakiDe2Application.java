d19944985b045ff9b4b3deedde9e1898b1d58dc1
Phạm Nguyễn Trí Vinh <115800576+vinh27012004@users.noreply.github.com>
2026-04-24 13:47:46 +0700

    PUSH LẦN 1

---
+++ dethigiuaki_de2/src/main/java/thigk1/phamnguyentrivinh/dethigiuaki_de2/DethigiuakiDe2Application.java
@@ -0,0 +1,13 @@
+package thigk1.phamnguyentrivinh.dethigiuaki_de2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DethigiuakiDe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DethigiuakiDe2Application.class, args);
+	}
+
+}
---
+++ dethigiuaki_de2/src/test/java/thigk1/phamnguyentrivinh/dethigiuaki_de2/DethigiuakiDe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk1.phamnguyentrivinh.dethigiuaki_de2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DethigiuakiDe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
